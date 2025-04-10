7aa55c42f53bd506f1b12fe0d7c939debff52734
Vitor Fidelis <109974299+VitorFidelis@users.noreply.github.com>
2025-04-09 22:03:43 -0300

    corrigindo as alterações em MsUserApplication e MsUserApplicationTests

--- ms-users/src/main/java/com/fiap/tech_challenge/parte1/ms_users/MsUsersApplication.java
+++ ms-users/src/main/java/com/fiap/tech_challenge/parte1/ms_users/MsUsersApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class MsUsersApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class,args);
+        SpringApplication.run(MsUsersApplication.class, args);
     }
 }
---
+++ ms-users/src/test/java/com/fiap/tech_challenge/parte1/ms_users/MsUsersApplicationTests.java
@@ -0,0 +1,12 @@
+package com.fiap.tech_challenge.parte1.ms_users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
