3599111d662225ccfb123ff36b1a41856c4b6723
Vitor Fidelis <109974299+VitorFidelis@users.noreply.github.com>
2025-04-09 22:21:27 -0300

    Refazendo o commit, pois esqueci de executar o git pull e sincronizar os arquivos do Docker

--- ms-users/src/main/java/com/fiap/tech_challenge/parte1/ms_users/MsUsersApplication.java
+++ ms-users/src/main/java/com/fiap/tech_challenge/parte1/ms_users/MsUsersApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsUsersApplication {
+public class MsUsersApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(MsUsersApplication.class, args);
