a49c635d53e5ac8af6be31531cb65a14d6f4f1c4
Gabriel Soares <gabrielalcantarars@gmail.com>
2025-04-18 23:43:08 -0300

    feat: Create local-continuous-integration.sh script and resolve Maven test issues
    - Added a new `local-continuous-integration.sh` script to automate the CI pipeline locally.
    - Script stops Docker Compose services, builds the Docker image, runs Maven tests, and restarts services.
    - Resolved Maven test issues by leveraging Docker with Maven 3.9.6 and Eclipse Temurin 17.
    - Persisted Maven dependencies in the host system's `.m2` directory to avoid redundant downloads.

--- ms-users/src/test/java/com/fiap/tech_challenge/parte1/ms_users/MsUsersApplicationTests.java
+++ ms-users/src/test/java/com/fiap/tech_challenge/parte1/ms_users/MsUsersApplicationTests.java
@@ -2,8 +2,10 @@ package com.fiap.tech_challenge.parte1.ms_users;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MsUsersApplicationTests {
 
     @Test
