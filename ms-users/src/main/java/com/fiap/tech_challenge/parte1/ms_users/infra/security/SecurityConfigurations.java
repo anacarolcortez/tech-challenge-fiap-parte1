a0990f87eae11016f05002a7cdd7dab098bfeb72
Eric Monné Fraga de Oliveira <ericmfo@hotmail.com>
2025-05-24 11:09:05 -0300

    Improving access control

--- ms-users/src/main/java/com/fiap/tech_challenge/parte1/ms_users/infra/security/SecurityConfigurations.java
+++ ms-users/src/main/java/com/fiap/tech_challenge/parte1/ms_users/infra/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package com.fiap.tech_challenge.parte1.ms_users.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,10 +13,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -27,7 +32,7 @@ public class SecurityConfigurations {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/login").permitAll()
                         .anyRequest().authenticated()
-                )
+                ).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
